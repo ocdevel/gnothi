aa750c500a262cba6c196be32854e0845e12876b
Tyler Renelle <tylerrenelle@gmail.com>
2019-11-12 07:12:31 +0100

    WIP entries

--- server/jwtauthtest/models.py
+++ server/jwtauthtest/models.py
@@ -1,5 +1,5 @@
 from jwtauthtest.database import Base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Text
 
 
 class User(Base):
@@ -14,5 +14,27 @@ class User(Base):
         self.password = password
 
     def __repr__(self):
-        return f'<User {{ username: {self.username}, \
-password: {self.password} }}'
+        return f'<User {{ username: {self.username}, password: {self.password} }}'
+
+
+class Entry(Base):
+    __tablename__ = 'entries'
+
+    id = Column(Integer, primary_key=True)
+    text = Column(Text, nullable=False)
+
+
+class MetaField(Base):
+    __tablename__ = 'meta_fields'
+
+    id = Column(Integer, primary_key=True)
+    type = [numeric, 5_star, bool, ...]
+    # References user
+
+
+class MetaEntries(Base):
+    __tablename__ = 'meta_entries'
+
+    id = Column(Integer, primary_key=True)
+    value = ??
+    # References Entry, MetaField
