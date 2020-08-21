b291f13107dfcdae49478ab5fba241898539f716
Tyler Renelle <tylerrenelle@gmail.com>
2020-08-21 10:55:46 -0700

    ec2: debounce ts_client as global python var

--- server/app/ec2_updown.py
+++ server/app/ec2_updown.py
@@ -1,4 +1,4 @@
-import boto3, time, threading, os
+import boto3, time, threading, os, datetime
 from app.utils import is_dev, vars
 from fastapi_sqlalchemy import db
 import socket
@@ -6,13 +6,13 @@ import socket
 # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ec2.html
 ec2_client = boto3.client('ec2')
 
-
 def _fetch_status():
     sql = """
     -- Ensure 1 row exists
     insert into jobs_status (id, status, ts_client, ts_svc, svc) 
         values (1, 'off', now(), now(), null) 
         on conflict (id) do nothing;
+    update jobs_status set ts_client=now();
     select status, svc,
         extract(epoch FROM (now() - ts_svc)) as elapsed_svc,
         extract(epoch FROM (now() - ts_client)) as elapsed_client
@@ -30,14 +30,15 @@ def ec2_up():
     except: pass
 
 
+debounce = [datetime.datetime.now(), None]
 def jobs_status():
-    res = _fetch_status()
+    global debounce
+
     # debounce client (race-condition, perf)
-    # TODO maybe cache it as global var, so not hitting db so much?
-    if res.elapsed_client < 2:
-        return res.status
-    db.session.execute("update jobs_status set ts_client=now()")
-    db.session.commit()
+    if debounce[1] and (datetime.datetime.now() - debounce[0]).total_seconds() < 3:
+        return debounce[1]
+    res = _fetch_status()
+    debounce[0], debounce[1] = datetime.datetime.now(), res
 
     # job service is fresh (5s)
     if res.elapsed_svc < 5: pass
