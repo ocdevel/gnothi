1d2258938291542cb329d3e05eac24a05522c63b
Tyler Renelle <tylerrenelle@gmail.com>
2020-10-14 19:40:37 -0400

    notify of important events

--- server/app/google_analytics.py
+++ server/app/google_analytics.py
@@ -1,5 +1,8 @@
 import requests
+from app.mail import send_mail
 from common.utils import vars, is_dev
+from common.database import session
+from sqlalchemy import text
 from typing import Union
 from pydantic import UUID4
 import hashlib
@@ -13,16 +16,25 @@ def ga(uid: Union[str, UUID4], category: str, action: str):
     """
     # actually don't care about uid, just need a unique identifier. Note this is
     # a 1-way hash (right?), so I can't even decrypt - just want unique per-feature track
-    uid = str(uid).encode()  # to bytes
-    uid = hashlib.sha256(uid).hexdigest()
+    uid_ = str(uid).encode()  # to bytes
+    uid_ = hashlib.sha256(uid_).hexdigest()
     url = "https://ssl.google-analytics.com/"
     url += "debug/collect" if DEBUG else "collect"
     res = requests.post(url, params=dict(
         v=1,
         tid=vars.GA,
-        cid=uid,
+        cid=uid_,
         t='event',
         ec=category,
         ea=action
     ))
     if DEBUG: print(res.json())
+
+    if action in ('register', 'like', 'dislike', 'therapist'):
+        with session() as sess:
+            if sess.execute(text("""
+            select is_superuser su from users where id=:uid
+            """), dict(uid=uid)).fetchone().su:
+                # don't notify of my own or Lisa's entries
+                return
+        send_mail('tylerrenelle@gmail.com', 'action', dict(category=category, action=action))
--- server/app/mail.py
+++ server/app/mail.py
@@ -6,9 +6,14 @@ url = "http://localhost:3002" if is_dev() else "https://gnothiai.com"
 def send_mail(email, type, data):
     if is_dev(): return
     if type == 'forgot-password':
-        body = f"Click this link to confirm your account: {url}/reset-password?token={data}"
+        subject = "Gnothi reset password"
+        body = f"Click this link to reset your password: {url}/reset-password?token={data}"
     elif type == 'welcome':
+        subject = "Welcome to Gnothi"
         body = f"Thank you for registering a Gnothi account! Start journaling away."
+    elif type == 'action':
+        subject = f"Gnothi: {data['category']}/{data['action']}"
+        body = subject
     else: return
 
     ses = boto3.client('ses')
@@ -16,7 +21,7 @@ def send_mail(email, type, data):
         Source=vars.EMAIL_SES_EMAIL_SOURCE,
         Destination={'ToAddresses': [email]},
         Message={
-            'Subject': {'Data': 'Confirm Your Account'},
+            'Subject': {'Data': subject},
             'Body': {
                 'Text': {'Data': body}
             }
