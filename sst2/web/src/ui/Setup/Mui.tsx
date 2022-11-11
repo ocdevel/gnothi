da1f816307e584df7afd9a46ac8fa9d38145221c
Martha Wilding <wilding34@gmail.com>
2022-11-11 10:28:54 -0800

    initial colors

--- sst2/web/src/ui/Setup/Mui.tsx
+++ sst2/web/src/ui/Setup/Mui.tsx
@@ -1,19 +1,85 @@
 import React from "react";
 import CssBaseline from "@mui/material/CssBaseline";
 import StyledEngineProvider from "@mui/material/StyledEngineProvider";
-import {createTheme, ThemeProvider} from "@mui/material/styles";
-import { green, purple } from '@mui/material/colors';
+import {createTheme, ThemeProvider, ThemeOptions} from "@mui/material/styles";
 
-const theme = createTheme({
-  // palette: {
-  //   primary: {
-  //     main: purple[500],
-  //   },
-  //   secondary: {
-  //     main: green[500],
-  //   },
-  // },
-})
+export const themeOptions: ThemeOptions = {
+  palette: {
+    primary: {
+      main: '#1c3f60',
+      light: '#738fa7',
+      contrastText: '#ffffff',
+    },
+    secondary: {
+      main: '#738fa7',
+      light: '#6ba9bb',
+    },
+    background: {
+      paper: '#f0efec',
+    },
+    text: {
+      primary: '#071330',
+    },
+    error: {
+      main: '#dc6783',
+    },
+    warning: {
+      main: '#dc6783',
+    },
+    success: {
+      main: '#50dece',
+    },
+  },
+  typography: {
+    subtitle1: {
+      fontFamily: 'Poppins',
+    },
+    subtitle2: {
+      fontFamily: 'Poppins',
+    },
+    body1: {
+      fontFamily: 'Poppins',
+    },
+    h3: {
+      fontWeight: 700,
+    },
+    h5: {
+      fontWeight: 900,
+    },
+    body2: {
+      fontFamily: 'Poppins',
+    },
+    caption: {
+      fontFamily: 'Poppins',
+    },
+    overline: {
+      fontFamily: 'Poppins',
+    },
+    h1: {
+      fontWeight: 800,
+      fontFamily: 'Antic Didone',
+    },
+    h2: {
+      fontWeight: 1000,
+    },
+    fontFamily: 'Antic Didone',
+  },
+  overrides: {
+    MuiAppBar: {
+      colorInherit: {
+        backgroundColor: '#738fa7',
+        color: '#fff',
+      },
+    },
+  },
+  props: {
+    MuiAppBar: {
+      color: 'inherit',
+    },
+  },
+};
+
+const theme = createTheme(themeOptions)
 
 export default function Mui({children}: React.PropsWithChildren<{}>) {
   // return <StyledEngineProvider injectFirst>
