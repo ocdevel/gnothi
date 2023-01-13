594e869bc0c471f29afb6581b65e4de3e9f5e8e2
Tyler Renelle <tylerrenelle@gmail.com>
2023-01-13 16:22:53 -0700

    prompt uses summary | entry | paragraphs

--- sst/services/data/models/insights.ts
+++ sst/services/data/models/insights.ts
@@ -7,5 +7,14 @@ import {boolMapToKeys} from '@gnothi/schemas/utils'
 import dayjs from "dayjs";
 
 export class Insights extends Base {
-
+  async entriesByIds(entry_ids: string[]) {
+    return db.executeStatement<S.Entries.Entry>({
+      sql: `select text_clean, ai_text, text_paras, text from entries 
+        where user_id=:user_id and id in :entry_ids order by created_at asc`,
+      parameters: [
+        {name: "user_id", typeHint: "UUID", value: {stringValue: this.uid}},
+        {name: "entry_ids", typeHint: "UUID", value: {arrayValue: {stringValues: entry_ids}}}
+      ]
+    })
+  }
 }
--- sst/services/routes/insights/insights_prompt.ts
+++ sst/services/routes/insights/insights_prompt.ts
@@ -6,22 +6,65 @@ import {z} from 'zod'
 // @ts-ignore
 import dayjs from 'dayjs'
 import {ulid} from 'ulid'
+import {Insights} from '../../data/models/insights'
+import {Entry} from '@gnothi/schemas/entries'
 
 const r = S.Routes.routes
 
+// prioritize clean-text, worst-case markdown
+function getText (e: Entry): string {
+  return e.text_clean || e.text
+}
+// prioritize summary, worst-case full-text
+function getSummary(e: Entry): string {
+  return e.ai_text || getText(e)
+}
+function getParas(e: Entry): string[] {
+  if (e.text_paras?.length) {
+    return e.text_paras
+  }
+  // TODO text_clean won't have paras, it's clean-join()'d, no paras preserved. This line
+  // should be rare though, since text_paras is likely available by now.
+  return getText(e).split(/\n+/)
+}
+
 r.insights_prompt_request.fn = r.insights_prompt_request.fnDef.implement(async (req, context) => {
-  const entries = await db.executeStatement<S.Entries.Entry>({
-    sql: "select text_clean, ai_text from entries where user_id=:user_id and id in :entry_ids order by created_at asc",
-    parameters: [
-      {name: "user_id", typeHint: "UUID", value: {stringValue: context.user.id}},
-      {name: "entry_ids", typeHint: "UUID", value: {arrayValue: {stringValues: req.entry_ids}}}
-    ]
-  })
-  const entry = entries.map(e => e.text_clean || e.ai_text || e.text).join('\n')
-  const prompt = req.prompt.replace("<entry>", entry)
-    .replace("<summary>", entry) // FIXME
-    .replace("<paragraphs>", entry) // FIXME
-  const response = await completion({prompt})
+  const mInsights = new Insights(context.user.id)
+  const entries = await mInsights.entriesByIds(req.entry_ids)
+  const {prompt, view} = req
+
+  const placeholder = {
+    entry: !!~prompt.indexOf('<entry>'),
+    paragraphs: !!~prompt.indexOf('<paragraphs>'),
+    summary: !!~prompt.indexOf('<summary>'),
+  }
+  const hasMultiple = Object.values(placeholder).filter(Boolean).length > 1
+  if (hasMultiple) {
+    throw new GnothiError({message: "Only one placeholder can be used at a time for now. This many change later."})
+  }
+
+  let response: string
+  if (placeholder.paragraphs) {
+    if (view === 'list') {
+      throw new GnothiError({message: "Can't yet use <paragraphs> in list view. When implemented, it will operate on themes."})
+    }
+    const paras = getParas(entries[0])
+    response = (await Promise.all(paras.map(async (p, i) => {
+      const completion_ = await completion({
+        prompt: prompt.replace("<paragraphs>", p),
+      })
+      return `Paragraph ${i}: ${completion_}`
+    }))).join('\n\n')
+  } else {
+    const texts = placeholder.summary ? entries.map(getSummary) : entries.map(getText)
+    const text = texts.join('\n')
+    response = await completion({
+      // entry v summary handled above, so just replace either/or here
+      prompt: prompt.replace("<entry>", text)
+        .replace("<summary>", text)
+    })
+  }
+
   return [{
     id: ulid(),
     view: req.view,
