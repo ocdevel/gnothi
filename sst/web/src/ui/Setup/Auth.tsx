e3fcb1a9bafaca506866d961c74a2dc0b3288d37
Tyler Renelle <tylerrenelle@gmail.com>
2023-01-27 15:55:05 -0800

    remove auth color overrides

--- sst/web/src/ui/Setup/Auth.tsx
+++ sst/web/src/ui/Setup/Auth.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useCallback, useState, FC} from 'react'
+import React, {useEffect, useCallback, useState, FC, memo} from 'react'
 import {Amplify, Auth} from 'aws-amplify'
 import {Authenticator, useAuthenticator, Theme, useTheme, ThemeProvider} from "@aws-amplify/ui-react";
 import {awsConfig} from '../../utils/config'
@@ -16,64 +16,13 @@ export function AuthComponent({tab}: AuthComponent) {
   // https://ui.docs.amplify.aws/react/connected-components/authenticator/customization
   // https://ui.docs.amplify.aws/react/theming#design-tokens
   // TODO customize colors / fonts
-  const { tokens } = useTheme();
-  const theme: Theme = {
-    name: 'Auth Example Theme',
-    tokens: {
-      colors: {
-        background: {
-          primary: {
-            value: tokens.colors.neutral['90'].value,
-          },
-          secondary: {
-            value: tokens.colors.neutral['100'].value,
-          },
-        },
-        font: {
-          interactive: {
-            value: tokens.colors.white.value,
-          },
-        },
-        brand: {
-          primary: {
-            '10': tokens.colors.teal['100'],
-            '80': tokens.colors.teal['40'],
-            '90': tokens.colors.teal['20'],
-            '100': tokens.colors.teal['10'],
-          },
-        },
-      },
-      components: {
-        tabs: {
-          item: {
-            _focus: {
-              color: {
-                value: tokens.colors.white.value,
-              },
-            },
-            _hover: {
-              color: {
-                value: tokens.colors.yellow['80'].value,
-              },
-            },
-            _active: {
-              color: {
-                value: tokens.colors.white.value,
-              },
-            },
-          },
-        },
-      },
-    },
-  };
+  // 177a5482 - sample theme
 
   // return <ThemeProvider theme={theme}>
-  return <ThemeProvider theme={theme}>
-    <Authenticator
-      loginMechanisms={["email"]}
-      initialState={tab}
-    />
-  </ThemeProvider>
+  return <Authenticator
+    loginMechanisms={["email"]}
+    initialState={tab}
+  />
   // return <Authenticator loginMechanisms={loginMechanisms}>
   //     {({ signOut, user }) => {
   //       return <Typography>Welcome ${user.username}</Typography>
