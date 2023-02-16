7f73b8769011befef265acbe3d79b8da313f5381
Tyler Renelle <tylerrenelle@gmail.com>
2023-02-16 13:03:36 -0800

    update staging vars for offline web

--- sst/web/src/utils/config.ts
+++ sst/web/src/utils/config.ts
@@ -3,7 +3,7 @@ export const STAGE = import.meta.env.VITE_STAGE
 const env = (
   // Using manually-entered values for staging env vars so we can develop localhost
   // against staging backend. Edge-case, won't use for production or other
-  STAGE === "staging" ? {"VITE_API_WS":"wss://gk25nph8o6.execute-api.us-east-1.amazonaws.com/staging","VITE_API_HTTP":"https://bel3k2njd8.execute-api.us-east-1.amazonaws.com","VITE_REGION":"us-east-1","VITE_USER_POOL_ID":"us-east-1_BJPuyWgb0","VITE_USER_POOL_CLIENT_ID":"282n7agovqicvma9rrti0nojo4"}
+  STAGE === "staging" ? {"VITE_API_WS":"wss://arkbead294.execute-api.us-east-1.amazonaws.com/staging","VITE_API_HTTP":"https://5meeddk8n5.execute-api.us-east-1.amazonaws.com","VITE_REGION":"us-east-1","VITE_USER_POOL_ID":"us-east-1_cI61ODnqv","VITE_USER_POOL_CLIENT_ID":"3eimk0p4blb9ch5lbilf2bh5dg"}
   // Allow editing static pages without need for a backend
   : STAGE === "offline" ? {
     "VITE_API_WS": "",
