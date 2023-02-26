b827e1146d2a243dcf3b1e5cd428810a24e23f19
Martha Wilding <wilding34@gmail.com>
2023-02-25 19:31:14 -0800

    step towards InputBase search + DatePickersˆ

--- sst/web/src/ui/App/Insights/Filters/Search.tsx
+++ sst/web/src/ui/App/Insights/Filters/Search.tsx
@@ -2,9 +2,20 @@ import React, {useCallback, useState, ChangeEventHandler} from "react";
 import debounce from "lodash/debounce";
 import SearchIcon from "@mui/icons-material/Search";
 import TextField from "@mui/material/TextField"
-import InputAdornment from "@mui/material/InputAdornment"
 import Box from "@mui/material/Box";
 import {useStore} from "../../../../data/store";
+import Paper from '@mui/material/Paper';
+import InputBase from '@mui/material/InputBase';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import CalendarIcon from '@mui/icons-material/CalendarMonthOutlined';
+import Stack from "@mui/material/Stack";
+import {Typography} from "@mui/material";
+
+import { Dayjs } from 'dayjs';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 
 /**
  * Entries() is expensive, so isolate <Search /> and only update the Entries.search
@@ -13,6 +24,7 @@ import {useStore} from "../../../../data/store";
 export default function Search() {
   // TODO use zodResolver for better performance
 
+  const filters = useStore(s => s.filters)
   const setFilters = useStore(s => s.setFilters)
   let [search, setSearch] = useState('')
 
@@ -28,22 +40,85 @@ export default function Search() {
     setSearch(search)
     trigger(search)
   }
-
   return <>
-    <TextField
-      placeholder="Search entries"
+  <Paper
+    elevation={1}
+    component="form"
+    sx={{
+      px: 2,
+      py: 1,
+      display: 'flex',
+      alignItems: 'center',
+      width: '100%',
+      backgroundColor: "white",
+      borderRadius: 3,
+    }}
+  >
+    <InputBase
       fullWidth
-      margin="normal"
-      InputProps={{
-        startAdornment: (
-          <InputAdornment position="start">
-            <SearchIcon />
-          </InputAdornment>
-        ),
-      }}
-      variant="standard"
       value={search}
       onChange={changeSearch}
+      sx={{ flex: 1 }}
+      placeholder="Search keywords and phrases, or ask a question"
+      inputProps={{ 'aria-label': 'Search keywords and phrases, or ask a question' }}
     />
+    {/*<IconButton type="button" sx={{ p: '10px' }} aria-label="search">
+      <SearchIcon />
+    </IconButton>*/}
+    {/*<Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />*/}
+    <Stack
+      direction="row"
+      spacing={2}
+      alignItems="center"
+    >
+      {/*<CalendarIcon />*/}
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <DatePicker
+          label="Start"
+          inputFormat="YYYY-MM-DD"
+          value={filters.startDate}
+          onChange={(startDate) => {
+            setFilters({startDate});
+          }}
+          disableFuture
+          renderInput={(params) => {
+            console.log(params)
+            return <TextField
+              {...params}
+              size="small"
+              sx={{maxWidth:150}}
+              variant="standard"
+              placeholder={undefined}
+              InputProps={{
+                ...params.InputProps,
+                disableUnderline: true
+              }}
+            />
+          }}
+        />
+        {/*<Typography variant="body1">-</Typography>*/}
+        <DatePicker
+          label="End"
+          inputFormat="YYYY-MM-DD"
+          value={filters.endDate === "now" ? undefined : filters.endDate}
+          onChange={(endDate) => {
+            setFilters({endDate});
+          }}
+          disableFuture
+          renderInput={(params) => <TextField
+            {...params}
+            size="small"
+            sx={{maxWidth:150}}
+            variant="standard"
+            placeholder={undefined}
+            InputProps={{
+                ...params.InputProps,
+                disableUnderline: true
+              }}
+          />}
+        />
+      </LocalizationProvider>
+    </Stack>
+  </Paper>
   </>
 }
--- sst/web/src/ui/App/Insights/Filters/index.tsx
+++ sst/web/src/ui/App/Insights/Filters/index.tsx
@@ -1,29 +1,43 @@
 import React, {useEffect, useState, useCallback} from 'react'
 import {MainTags} from "../../Tags/Tags";
 import Search from "./Search";
-import DatePickers from './DatePickers'
 import {useStore} from "../../../../data/store";
 import Box from "@mui/material/Box";
 import Stack from "@mui/material/Stack";
+import Grid from '@mui/material/Grid';
 
 export default function Filters() {
   const selectedTags = useStore(s => s.selectedTags)
   const filters = useStore(s => s.filters)
 
-  return <>
-    <Stack
-      spacing={2}
-      direction="row"
-      alignItems="center"
-      justifyContent="flex-start"
-      flexWrap="wrap"
+  return <Box
+    paddingTop={0}
+    paddingRight={5}
+    paddingLeft={5}
+  >
+    <Grid
+      container
+      direction="column"
+      // alignItems="center"
+      marginX={0}
     >
-      <DatePickers />
-      <Box
-       sx={{paddingTop:2}}>{MainTags}</Box>
-    </Stack>
-    <Box>
-      <Search />
-    </Box>
-  </>
+      <Grid
+        item
+        alignItems="flex-start"
+        justifyItems="flex-start"
+        marginX={-3}
+        sx={{paddingY: 2}}
+      >
+        <Search/>
+      </Grid>
+      <Grid
+        item
+        flexWrap="wrap"
+        marginX={-3}
+        sx={{paddingY: 2}}
+      >
+        {MainTags}
+      </Grid>
+    </Grid>
+  </Box>
 }
