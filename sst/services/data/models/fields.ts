65814537711d64151d5c4f68fb299778a4590d90
Tyler Renelle <tylerrenelle@gmail.com>
2023-03-14 13:47:29 -0700

    drizzle: WIP migrating fields

--- sst/services/data/models/fields.ts
+++ sst/services/data/models/fields.ts
@@ -2,12 +2,41 @@ import {Base} from './base'
 import {db} from '../db'
 import * as S from '@gnothi/schemas'
 import {sql} from "drizzle-orm/sql"
+import { and, asc, desc, eq, or } from 'drizzle-orm/expressions';
+import { pgTable, index, text, varchar, uuid, pgEnum, doublePrecision, timestamp, json } from 'drizzle-orm/pg-core';
+
+const fieldTypes = pgEnum('field_type', ["number", "fivestar", "check", "option"])
+const defaultValTypes = pgEnum('default_value_type', ["value", "average", "ffill"])
+const t = pgTable('fields', {
+  id: uuid('id').defaultRandom().notNull().primaryKey(),
+  type: fieldTypes("type").default("fivestar"),
+  name: varchar('name').notNull(),
+  created_at: timestamp('created_at', {withTimezone: true}).defaultNow(),
+  excluded_at: timestamp('excluded_at', {withTimezone: true}),
+  default_value: defaultValTypes("default_value").default("ffill"),
+  default_value_value: doublePrecision('default_value_value'),
+  attributes: json("attributes"),
+  service: varchar('service'),
+  service_id: varchar('service_id'),
+  user_id: uuid('user_id').notNull(),//.references(() => users.id, {onDelete: 'cascade'})
+  influencer_score: doublePrecision("influencer_score").default(0),
+  next_pred: doublePrecision("next_pred").default(0),
+  avg: doublePrecision("avg").default(0),
+}, (table) => {
+  return {
+    ix_fields_excluded_at: index("ix_fields_excluded_at").on(table.excluded_at),
+    ix_fields_user_id: index("ix_fields_user_id").on(table.user_id),
+    ix_fields_created_at: index("ix_fields_created_at").on(table.created_at),
+  }
+})
+
 
 export class Fields extends Base {
+  table
   async list() {
-    return db.query<S.Fields.fields_list_response>(
-      sql`select * from fields where user_id=${this.uid}`
-    )
+    const client = await db.client()
+    const res = await client.select().from(t).where(eq(t.user_id, this.uid))
+    return res.map(db._removeNull)
   }
 
   async post(req: S.Fields.fields_post_request) {
