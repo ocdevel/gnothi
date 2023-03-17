4fca5cbea2f50823a8f1cce1d2f92c1f5846836d
Tyler Renelle <tylerrenelle@gmail.com>
2023-03-17 15:52:18 -0700

    await db connect top-level (works node16+)

--- sst/services/auth/postConfirmation.ts
+++ sst/services/auth/postConfirmation.ts
@@ -2,17 +2,20 @@ import {db} from '../data/db'
 import * as S from "@gnothi/schemas"
 const testing = process.env.IS_LOCAL
 import {sql} from 'drizzle-orm/sql'
+import {users} from '../data/models/users'
 
 export const handler = async (event, context, callback) => {
   // create user in database. maybe add its uid to cognito
-  const dbUser = await db.queryFirst<S.Users.User>(
-    sql`insert into users (email, cognito_id) values (${event.request.userAttributes.email}, ${event.userName}) returning *;`
-  )
-  event.request.userAttributes['gnothiId'] = dbUser.id
+  const dbUser = await db.client.insert(users).values({
+    email: event.request.userAttributes.email,
+    cognito_id: event.userName,
+  }).returning()
+  const uid = dbUser[0].id
+  event.request.userAttributes['gnothiId'] = uid
 
   // All users need one immutable main tag
   const mainTag = await db.query(
-    sql`insert into tags (user_id, name, main) values (${dbUser.id}, 'Main', true)`
+    sql`insert into tags (user_id, name, main) values (${uid}, 'Main', true)`
   )
 
   return event
--- sst/services/data/db.ts
+++ sst/services/data/db.ts
@@ -5,102 +5,76 @@ import { pgTable, serial, text, varchar } from 'drizzle-orm/pg-core';
 import { drizzle, NodePgDatabase } from 'drizzle-orm/node-postgres'
 import { sql, SQL } from 'drizzle-orm/sql'
 
-import {z} from 'zod'
-import * as S from '@gnothi/schemas'
-// @ts-ignore
-import dayjs from 'dayjs'
 import {readFileSync} from "fs";
+
+const stagename = `gnothi${process.env.stage}`
+
+// set localhost defaults, override if deployed
+let secretValues = {
+  host: "localhost",
+  port: 5432,
+  username: "postgres",
+  password: "password"
+}
+if (!process.env.IS_LOCAL) {
+  // get the secret from secrets manager.
+  const secretsClient = new SecretsManagerClient({})
+  const secret = await secretsClient.send(new GetSecretValueCommand({
+    SecretId: process.env.rdsSecretArn,
+  }))
+  secretValues = JSON.parse(secret.SecretString ?? '{}')
+}
+
+// TODO use env.stage as db name
+// TODO initialize DB if not exists
+console.log('secreteKeys', Object.keys(secretValues))
+// const pgClient = new PgClient({
+const pgClient = new Pool({
+  max: 1, min: 0, // single connection for singleton Lambda
+  host: secretValues.host, // host is the endpoint of the db cluster
+  port: secretValues.port, // port is 5432
+  user: secretValues.username, // username is the same as the secret name
+  password: secretValues.password, // this is the password for the default database in the db cluster
+  database: stagename
+})
+// await pgClient.connect()
+const drizzleClient = drizzle(pgClient)
+
+// See https://gist.github.com/streamich/6175853840fb5209388405910c6cc04b
+// If using Pool (not Client), we can forgo connect() and the pool will self-manage.
+// Otherwise, we'd need:
+// const client = await pool.connect()
+// try {await client.query(q)}
+// finally {client.release(true)}
 
 
 export class DB {
   // private variable which will be singleton-initialized for running lambda (for all
   // future invocations). Note use of this class exported as an instance (bottom of file),
   // not instantiated by callers
-  private client_: NodePgDatabase | undefined
+  client = drizzleClient
   private dbName: string
-
-  constructor(dbName: string) {
-    if (!dbName) {
-      throw new Error('dbName is required. Would use postgres as default, but that could cause issues')
-    }
-    this.dbName = dbName // || 'postgres'
-  }
 
   static prepLambda(context: Context) {
     // https://gist.github.com/streamich/6175853840fb5209388405910c6cc04b
     context.callbackWaitsForEmptyEventLoop = false; // !important to reuse pool
   }
 
-  static stagename() {
-    return `gnothi${process.env.stage}`
-  }
-
-  async createClient(): Promise<NodePgDatabase> {
-    // set localhost defaults, override if deployed
-    let secretValues = {
-      host: "localhost",
-      port: 5432,
-      username: "postgres",
-      password: "password"
-    }
-    if (!process.env.IS_LOCAL) {
-      // get the secret from secrets manager.
-      const secretsClient = new SecretsManagerClient({})
-      const secret = await secretsClient.send(new GetSecretValueCommand({
-        SecretId: process.env.rdsSecretArn,
-      }))
-      secretValues = JSON.parse(secret.SecretString ?? '{}')
-    }
-
-
-    // TODO use env.stage as db name
-    // TODO initialize DB if not exists
-    console.log('secreteKeys', Object.keys(secretValues))
-    // const db = new PgClient({
-    const client = new Pool({
-      max: 1, min: 0, // single connection for singleton Lambda
-      host: secretValues.host, // host is the endpoint of the db cluster
-      port: secretValues.port, // port is 5432
-      user: secretValues.username, // username is the same as the secret name
-      password: secretValues.password, // this is the password for the default database in the db cluster
-      database: this.dbName,
-    })
-    // await client.connect()
-    const db = drizzle(client)
-
-    // See https://gist.github.com/streamich/6175853840fb5209388405910c6cc04b
-    // If using Pool (not Client), we can forgo connect() and the pool will self-manage.
-    // Otherwise, we'd need:
-    // const client = await pool.connect()
-    // try {await client.query(q)}
-    // finally {client.release(true)}
-
-    return db
-  }
-
-  async client(): Promise<NodePgDatabase> {
-    if (!this.client_) {
-      this.client_ = await this.createClient()
-    }
-    return this.client_
-  }
-
-  _removeNull(obj: object) {
+  removeNull(obj: object) {
     // Returned zod objects with optional fields only allow `null` if `.nullable()`
     // is used. This strips nulls from the object. Keep an eye on this, there may
     // be a future situation in which null is a meaningful value over undefined.
     return Object.fromEntries(Object.entries(obj).filter(([_, v]) => v !== null));
   }
 
-  _removeUndefined(obj: object) {
+  removeUndefined(obj: object) {
     return Object.fromEntries(Object.entries(obj).filter(([_, v]) => v !== undefined));
   }
 
   async query<O = any>(sql_: SQL): Promise<O[]> {
     try {
-      const client = await this.client()
-      const queryResult: QueryResult<O> = await client.execute<O>(sql_)
-      return queryResult.rows.map(this._removeNull)
+      const queryResult: QueryResult<O> = await drizzleClient.execute<O>(sql_)
+      return queryResult.rows.map(this.removeNull)
     } catch (error) {
       // (await this.client()).release(true)
       console.error({error, sql_})
@@ -118,7 +92,7 @@ export class DB {
   }
 
   async insert<O = object>(table: string, obj: object): Promise<O> {
-    const clean = this._removeUndefined(obj)
+    const clean = this.removeUndefined(obj)
     const keys = Object.keys(clean)
     const values = keys.map(k => clean[k])
     const sql = `insert into ${table} (${keys.join(', ')}) values (${keys.map((_, i) => `$${i+1}`).join(', ')}) returning *`
@@ -126,4 +100,4 @@ export class DB {
   }
 }
 
-export const db = new DB(DB.stagename())
+export const db = new DB()
